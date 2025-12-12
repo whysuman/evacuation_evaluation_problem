0a33611e6a7599c0b4a91112ebc0aeba988dc58f
SumanthraAiden <sumanthra.yerrabelly@aidenai.com>
2025-12-11 21:49:53 -0500

    Added the working visualisatoin

--- visualisation/src/App.jsx
+++ visualisation/src/App.jsx
@@ -1,11 +1,12 @@
-import BuildingEvacuationViz from './components/BuildingEvacuationViz'
+import React from 'react';
+import BuildingEvacuationViz from './components/BuildingEvacuationViz.jsx';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 py-8">
       <BuildingEvacuationViz />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
